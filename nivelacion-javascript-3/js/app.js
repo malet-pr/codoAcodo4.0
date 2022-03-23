b1ff7a92055938b60c54ebc3ede9bcbd4ce6ec58
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-03-22 21:35:04 -0300

    agregado redondeo

--- nivelacion-javascript-3/js/app.js
+++ nivelacion-javascript-3/js/app.js
@@ -19,10 +19,10 @@ function process1() {
     if(r < 0){
         alert("El radio no puede ser negativo");
     } else if(r > 100) {
-        alert("El rango supera la capacidad de calculo, debe elegir entre 0 y 100");
+        alert("El rango supera la capacidad de cálculo, debe elegir entre 0 y 100");
     } else {
-        alert("El área del círculo es: " + circleArea(r));
-        document.getElementById('out1').innerHTML = "El área del círculo es: " +  circleArea(r);
+        alert("El área del círculo es: " + round(circleArea(r),2));
+        document.getElementById('out1').innerHTML = "El área del círculo es: " +  round(circleArea(r),2);
     }
 }
 
@@ -32,11 +32,11 @@ function process2(){
     let l2 = document.getElementById("side2").value;
     if((l1 < 0 | l2 < 0) && (l1 > 100 | l2 > 100)){
         alert("El largo de los lados no puede ser negativo \n" +
-              "El rango supera la capacidad de calculo, los lados deben ser menores a 100");
+              "El rango supera la capacidad de cálculo, los lados deben ser menores a 100");
     } else if(l1 < 0 | l2 < 0){
         alert("El largo de los lados no puede ser negativo");
     } else if(l1 > 100 | l2 > 100) {
-        alert("El rango supera la capacidad de calculo, los lados deben ser menores a 100");
+        alert("El rango supera la capacidad de cálculo, los lados deben ser menores a 100");
     } else {
         alert("El área del rectángulo es: " + rectangleArea(l1,l2));
         document.getElementById('out2').innerHTML = "El área del rectángulo es: " + rectangleArea(l1,l2);
@@ -48,11 +48,11 @@ function process3(){
     let b = document.getElementById("base").value;
     if((h < 0 | b < 0) && (h > 100 | b > 100)){
         alert("La base y la altura no puede ser negativas \n" +
-              "El rango supera la capacidad de calculo, base y altura deben ser menores a 100");
+              "El rango supera la capacidad de cálculo, base y altura deben ser menores a 100");
     } else if(h < 0 | b < 0){
         alert("La base y la altura no puede ser negativas");
     } else if(h > 100 | b > 100) {
-        alert("El rango supera la capacidad de calculo, base y altura deben ser menores a 100");
+        alert("El rango supera la capacidad de cálculo, base y altura deben ser menores a 100");
     } else {
         alert("El área del triángulo es: " + triangleArea(b,h));
         document.getElementById('out3').innerHTML = "El área del triángulo es: " + triangleArea(b,h);
@@ -70,4 +70,10 @@ function rectangleArea(side1,side2){
 
 function triangleArea(height,base){
     return height*base*0.5;
+}
+
+function round(num, decimalPlaces = 0) {
+    var p = Math.pow(10, decimalPlaces);
+    var n = (num * p) * (1 + Number.EPSILON);
+    return Math.round(n) / p;
 }
