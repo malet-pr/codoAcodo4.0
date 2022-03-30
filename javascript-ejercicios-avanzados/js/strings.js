4e10ad5ed20848a42c18241936d2d57c0211d64f
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-03-30 01:17:10 -0300

    added string.js exercises

--- javascript-ejercicios-avanzados/js/strings.js
+++ javascript-ejercicios-avanzados/js/strings.js
@@ -1,20 +1,59 @@
+// esto es para poder usar prompt
+"use strict";
+const prompt = require('prompt-sync')({sigint:true});
+
 /* Ejercicios
 1. Realiza un script que pida un texto y lo muestre en mayúsculas.
 Investigar e implementar:  .toUpperCase */
 
+let text = prompt("Escriba un texto: ");
+let newText = text.toUpperCase();
+console.log(newText);
 
 /* 2. Realiza un script que cuente el número de vocales que tiene un texto. */
 
+const vowels = ['a','e','i','o','u'];
+let text2 = prompt("Escriba otro texto: ");
+let str = text2.toLowerCase().split(" ");
+let count = 0;
+for(let i=0;i<str.length;i++){
+    for(let char of str[i]){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+}
+console.log(count);
 
 
-/*     3. Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”. */
+/* 3. Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, 
+si tecleo “hola que tal” deberá mostrar “lat euq aloh”. */
 
-
+let text3 = prompt("Escriba un nuevo texto: ");
+console.log(text3.split("").reverse().join(""));
 
 /* 4. Realiza un juego de Piedra Papel Tijera contra Gana ARBI. */
 
+// no se que es Gana ARBI pero se me ocurre que puede se ésto
 
-
+let options = ["piedra","papel","tijera"];
+let choice = prompt("¿Piedra, papel o tijera? (o salir para terminar) : ");
+while(choice.toLowerCase() != "salir"){
+    switch (choice.toLowerCase()){
+        case 'piedra':
+            console.log('papel');
+            break;
+        case 'papel':
+            console.log("tijera");
+            break;
+        case 'tijera':
+            console.log("piedra");
+            break;
+        default:
+            console.log("tiene que elegir una de las opciones: piedra, papel o tijera");
+    }
+    choice = prompt("¿Piedra, papel o tijera? : ");
+} 
 
 
 
