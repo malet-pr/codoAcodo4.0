9e103ee30affe3b519f03a269d35723d82ef4a7d
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-03-30 02:00:58 -0300

    added excercise match-dowhile.js

--- javascript-ejercicios-avanzados/js/match-dowhile.js
+++ javascript-ejercicios-avanzados/js/match-dowhile.js
@@ -1,17 +1,29 @@
+// esto es para poder usar prompt
+"use strict";
+const prompt = require('prompt-sync')({sigint:true});
+
 /* Ejercicios
 
 1. Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor 
   //Función max() devuelve el mayor de los argumentos pasados.
   Ejemplo Math.max(edad1, edad2, edad3)
 */
-  
-
-
+let names = [];
+let ages = [];
+console.log("Se pedirán tres nombres con sus respectivas edades.")
+for(let i = 0; i < 3; i++){
+    let name = prompt("Nombre: ");
+    let age = prompt("Edad: ");
+    names.push(name);
+    ages.push(parseInt(age));
+}
+let maxAge = Math.max(ages[0],ages[1],ages[2]);
+console.log(`La persona de mayor edad es ${names[ages.indexOf(maxAge)]} y tiene ${maxAge} años.`);
 
 /*   2. Realiza un script que genere un número aleatorio entre 1 y 99 */
     //FUNCIÓN Math.floor: redondea al número entero menor.
     //FUNCIÓN Math.random: devuelve un número aleatorio mayor que 0 y menor que 1
-    
+console.log(Math.floor(Math.random() * 99) + 1);
 
 
 
