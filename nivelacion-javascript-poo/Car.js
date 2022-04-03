c0e21c167eac82f7070389d832305c660e6912ec
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-04-03 01:40:25 -0300

    fixed carousel

---
+++ nivelacion-javascript-poo/Car.js
@@ -0,0 +1,34 @@
+class Car {
+
+    constructor() {}
+    
+    constructor(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto) {
+        this.cantidadRuedas = cantidadRuedas;
+        this.cantidadPuertas = cantidadPuertas;
+        this.tipoCarroceria = tipoCarroceria;
+        this.modelo = modelo;
+        this.marcaDestino = marcaDestino;
+        this.costoFabricacion = costoFabricacion;
+        this.apto = apto;
+    }
+
+    //TODO: completar los métodos
+
+    encenderMotor(){}
+    apagarMotor(){}
+    mover(){}
+    frenar(){}
+    acelerar(){}
+    encenderLuces(){}
+    apagarLuces() {}
+    tocarBocina(){}
+    
+    probarAuto(){}
+
+    listaAutosFabricados(){}
+    agregarAuto(){}
+
+    toString(){}
+
+}
+
---
+++ nivelacion-javascript-poo/LuxuryCar.js
@@ -0,0 +1,17 @@
+class LuxuryCarr extends Car{
+
+    constructor(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto,
+         techoCorredizo, asientoCuero, patenteLujo) {
+        super(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto);
+        this.techoCorredizo = techoCorredizo;
+        this.asientoCuero = asientoCuero;
+        this.patenteLujo = patenteLujo;
+    }
+
+    //TODO: completar los métodos
+
+    toString(){}
+
+    probarAuto(){}
+
+}
