81e0796bcfcdec3425fecd2547fe2a5b3e0ff25a
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-04-10 14:42:04 -0300

    advances in poo section

--- nivelacion-javascript-poo/Car.js
+++ nivelacion-javascript-poo/Car.js
@@ -1,7 +1,8 @@
 class Car {
+    "use stric";
 
-    constructor() {}
-    
+    constructor(){}
+
     constructor(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto) {
         this.cantidadRuedas = cantidadRuedas;
         this.cantidadPuertas = cantidadPuertas;
@@ -12,23 +13,87 @@ class Car {
         this.apto = apto;
     }
 
-    //TODO: completar los métodos
-
-    encenderMotor(){}
-    apagarMotor(){}
-    mover(){}
-    frenar(){}
-    acelerar(){}
-    encenderLuces(){}
-    apagarLuces() {}
-    tocarBocina(){}
+    encenderMotor(test){
+        if(test == true){
+            console.log('Se encendió el motor');
+        }
+        return test;
+    }
+    apagarMotor(test){
+        if(test == true){
+            console.log('Se apagó el motor');
+        }
+        return test;
+    }
+    mover(test){
+        if(test == true){
+            console.log('El auto está en movimiento');
+        }
+        return test;
+    }
+    frenar(test){
+        if(test == true){
+            console.log('El auto frenó');
+        }
+        return test;
+    }
+    acelerar(test){
+        if(test == true){
+            console.log('El auto aceleró de 0 a 50');
+        }
+        return test;
+    }
+    encenderLuces(test){
+        if(test == truetrue){
+            console.log('Se encendieron las luces');
+        }
+        return test;
+    }
+    apagarLuces(test) {
+        if(test == true){
+            console.log('Se apagaron las luces');
+        }
+        return test;
+    }
+    tocarBocina(test){
+        if(test == true){
+            console.log('Sonó la bocina');
+        }
+        return test;
+    }
     
-    probarAuto(){}
+    probarAuto(auto){
+        if(auto.encenderMotor(test) && auto.apagarMotor(test) && auto.mover(test) && auto.frenar(test) && 
+            auto.acelerar(test) && auto.encenderLuces(test) && auto.apagarLuces(test) && auto.tocarBocina(test)){
+            auto.apto = true;
+        }
+        auto.apto = false;
+    }
 
-    listaAutosFabricados(){}
-    agregarAuto(){}
+    listarAutos(arrAutos){
+        let result = [];
+        for(let auto in arrAutos){
+            result.push(`${auto}: es modelo ${auto.modelo}, para la marca ${auto.marcaDestino},
+                        y tiene carrocería ${auto.tipoCarroceria}.`);
+        }
+        return result;
+    }
 
-    toString(){}
+    agregarAuto(auto,listaAutos){
+        listaAutos.push(`${auto}: es modelo ${auto.modelo}, para la marca ${auto.marcaDestino},
+                        y tiene carrocería ${auto.tipoCarroceria}.`);
+        return listaAutos;
+    }
+
+    toString(){
+        retrun
+        `El auto tiene ruedas: ${this.cantidadRuedas!=null ? this.cantidadRuedas : "desconocida"}, 
+            puertas: ${this.cantidadPuertas!=null ? this.cantidadPuertas : "desconocida"},
+            carrocería: ${this.tipoCarroceria!=null ? this.tipoCarroceria : "desconocida"},
+            su modelo es: ${this.modelo!=null ? this.modelo : "desconocido"},
+            se fabricó para la marca: ${this.marcaDestino!=null ? this.marcaDestino : "desconocida"},
+            su costo de fabricación es: ${this.costoFabricacion!=null ? this.costoFabricacion : "desconocido"},
+            la prueba dio como resultado que ${this.apto==true ? "es apto." : "no es apto."}`;
+    }
 
 }
-
--- nivelacion-javascript-poo/LuxuryCar.js
+++ nivelacion-javascript-poo/LuxuryCar.js
@@ -1,11 +1,12 @@
 class LuxuryCarr extends Car{
 
     constructor(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto,
-         techoCorredizo, asientoCuero, patenteLujo) {
+         techoCorredizo, asientoCuero, patenteLujo, equipado) {
         super(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto);
         this.techoCorredizo = techoCorredizo;
         this.asientoCuero = asientoCuero;
         this.patenteLujo = patenteLujo;
+        this.equipado = equipado;
     }
 
     //TODO: completar los métodos
