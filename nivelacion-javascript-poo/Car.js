4a3eca76489dfd94ed76bdd2dfe7d78058d163c2
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-04-10 16:07:05 -0300

    refactored class Car

--- nivelacion-javascript-poo/Car.js
+++ nivelacion-javascript-poo/Car.js
@@ -3,14 +3,44 @@ class Car {
 
     constructor(){}
 
-    constructor(cantidadRuedas,cantidadPuertas,tipoCarroceria,modelo,marcaDestino,costoFabricacion,apto) {
+    constructor(cantidadRuedas,cantidadPuertas,marcaDestino,probado = false) {
         this.cantidadRuedas = cantidadRuedas;
         this.cantidadPuertas = cantidadPuertas;
-        this.tipoCarroceria = tipoCarroceria;
-        this.modelo = modelo;
         this.marcaDestino = marcaDestino;
-        this.costoFabricacion = costoFabricacion;
-        this.apto = apto;
+        this.setModelo(cantidadRuedas);
+        this.setTipoCarroceria(cantidadPuertas);
+        this.setCostoFabricacion(this.tipoCarroceria);
+        this.probado = probado;
+    }
+
+    setModelo(cantidadRuedas){
+        if(cantidadRuedas==2){
+            this.modelo = 'moto';
+        } else if (cantidadRuedas==4){
+            this.modelo = 'auto';
+        } else {
+            this.modelo = 'camioneta';
+        }
+    }
+
+    setTipoCarroceria(cantidadPuertas){
+        if(cantidadPuertas < 4){
+            this.tipoCarroceria = 'chica';
+        } else if (cantidadPuertas > 3 && car.cantidadPuertas < 6){
+            this.tipoCarroceria = 'mediana';
+        } else {
+            this.tipoCarroceria = 'grande';
+        }
+    }
+
+    setCostoFabricacion(tipoCarroceria){
+        if(tipoCarroceria=='chica'){
+            this.costoFabricacion = (50000 + 5000);
+        } else if (tipoCarroceria=='mediana'){
+            this.costoFabricacion = (50000^2 + 85000);
+        } else {
+            this.costoFabricacion = ((50000^2 + 85000)*5 + 180000);
+        }
     }
 
     encenderMotor(test){
@@ -62,38 +92,50 @@ class Car {
         return test;
     }
     
-    probarAuto(auto){
-        if(auto.encenderMotor(test) && auto.apagarMotor(test) && auto.mover(test) && auto.frenar(test) && 
-            auto.acelerar(test) && auto.encenderLuces(test) && auto.apagarLuces(test) && auto.tocarBocina(test)){
-            auto.apto = true;
+    probarAuto(){
+        this.probado = true;
+        if(this.encenderMotor(test) && this.apagarMotor(test) && this.mover(test) && this.frenar(test) && 
+            this.acelerar(test) && this.encenderLuces(test) && this.apagarLuces(test) && this.tocarBocina(test)){
+            this.apto = true;
+        } else {
+            this.apto = false;
         }
-        auto.apto = false;
     }
 
-    listarAutos(arrAutos){
+    listarAutos(arrCars){
         let result = [];
-        for(let auto in arrAutos){
-            result.push(`${auto}: es modelo ${auto.modelo}, para la marca ${auto.marcaDestino},
-                        y tiene carrocería ${auto.tipoCarroceria}.`);
+        for(let car in arrCars){
+            result.push(`${car}: es modelo ${car.modelo}, para la marca ${car.marcaDestino},
+                        y tiene carrocería ${car.tipoCarroceria}.`);
         }
         return result;
     }
 
-    agregarAuto(auto,listaAutos){
-        listaAutos.push(`${auto}: es modelo ${auto.modelo}, para la marca ${auto.marcaDestino},
-                        y tiene carrocería ${auto.tipoCarroceria}.`);
-        return listaAutos;
+    agregarAuto(car,carList){
+        listaAutos.push(`${car}: es modelo ${car.modelo}, para la marca ${car.marcaDestino},
+                        y tiene carrocería ${car.tipoCarroceria}.`);
+        return carList;
     }
 
-    toString(){
+    toString(test){
+        let status;
+        if(this.probado == false){
+            status = 'no fue probado todavía';
+        } else {
+            if(this.apto == false){
+                status = 'no es apto';
+            } else {
+            status = 'es apto';
+            }
+        }
         retrun
         `El auto tiene ruedas: ${this.cantidadRuedas!=null ? this.cantidadRuedas : "desconocida"}, 
             puertas: ${this.cantidadPuertas!=null ? this.cantidadPuertas : "desconocida"},
             carrocería: ${this.tipoCarroceria!=null ? this.tipoCarroceria : "desconocida"},
             su modelo es: ${this.modelo!=null ? this.modelo : "desconocido"},
             se fabricó para la marca: ${this.marcaDestino!=null ? this.marcaDestino : "desconocida"},
-            su costo de fabricación es: ${this.costoFabricacion!=null ? this.costoFabricacion : "desconocido"},
-            la prueba dio como resultado que ${this.apto==true ? "es apto." : "no es apto."}`;
+            su costo de fabricación es: ${this.costoFabricacion!=null ? this.costoFabricacion : "desconocido"}.
+            El auto ${status}.`;
     }
 
-}
+
