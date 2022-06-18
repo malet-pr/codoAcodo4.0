aefab23a80ddb047e1ad613dd819b8cd2eab088d
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2022-06-18 15:55:40 -0300

    mejora de estilos

--- tp-individual/src/TareasItem.js
+++ tp-individual/src/TareasItem.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
-import {IoCheckmarkSharp,IoCloseCircleOutline} from "react-icons/io5";
+import {IoCheckmarkSharp,IoCheckmarkDoneSharp,IoCloseCircleOutline} from "react-icons/io5";
 import "./TareasItem.css";
 
 
 function TareasItem(props) {
 
+  function Completado() {
+    const completado = props.completado;
+    return completado ?  <IoCheckmarkDoneSharp /> : <IoCheckmarkSharp onClick={onCompletado}/> ;
+  }
+
   const onCompletado = ()=>{
-    alert (`Completaste la tarea ${props.nombre}`)
+    alert (`Completaste la tarea ${props.nombre}`);
   }
 
   const onBorrado = () =>{
-    alert (`Borraste la tarea ${props.nombre}`)
+    alert (`Borraste la tarea ${props.nombre}`);
   }
 
   return [
     <ListGroup.Item  className="d-flex justify-content-between align-items-start" id="stripped" >
-      
-      <span className={`Icon Icon-check ${props.completado &&'Icon-check--active'}`} onClick={onCompletado}>
-        <IoCheckmarkSharp/>
+      <span className={`Icon Icon-check ${props.completado && ' Icon-check--completado'}`}>
+        <Completado />
       </span>
       <p className={`TareasItem-p ${props.completado && 'TareasItem-p--completado'}`}>
         {props.nombre} 
